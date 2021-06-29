8922b2b961b02f8ee6afc02b69e008a7221bbbf0
flavioguisantos <flavio.guilherme@outlook.com.br>
2021-06-29 17:09:58 -0300

    chore: jestconfig

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
-export default {
+module.exports = {
+
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
@@ -6,4 +7,14 @@ export default {
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
+
+  // Automatically clear mock calls and instances between every test
+  // clearMocks: true
+
+  // Indicates whether the coverage information should be collected while executing the test
+  // collectCoverage: true,
+
+  // Indicates which provider should be used to instrument code for coverage
+  // coverageProvider: "v8",
+
 }
